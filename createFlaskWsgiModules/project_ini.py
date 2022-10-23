a8d93c3727f035c63fc5151fca72f416caa52b15
lewismunene <munene.lewis@students.jkuat.ac.ke>
2022-10-23 14:49:29 +0300

    setting up  wsgi support  module

--- createFlaskWsgiModules/project_ini.py
+++ createFlaskWsgiModules/project_ini.py
@@ -6,7 +6,7 @@ __DIR__ = argv[1]
 
 def  makeProjectIni(directory):
     data ="[uwsgi]\\nmodule = wsgi:app\\nmaster = true\\n"
-    data+="processes = 5\\n"
+    data+="processes = 3\\n"
     data+="socket =  project.sock \\nchmod-socket = 660 \\n"
     data+="vacuum = true\\n"
     data+="die-on-term = true\\n"
