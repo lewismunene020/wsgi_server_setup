b042e06f26faca09257e08d3f843b7d638d0a5db
lewismunene <munene.lewis@students.jkuat.ac.ke>
2022-12-05 19:51:13 +0300

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 # echo -e "installing ......." 
 #  copying  the  createFlaskWsgi  folder to  /usr/local/share
-cp -r createFlaskWsgi /usr/local/share/
+cp -r ./createFlaskWsgi /usr/local/share/
 #  copying  the  createwsgi  script to  /usr/local/bin
-cp -r createwsgi /usr/local/bin
+cp -r ./createflaskwsgi /usr/local/bin
 
 echo -e "$GREEN flask wsgi script setup  installed successfully $NC";
 
